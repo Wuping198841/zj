a15a6b17cb86d1b65704b9d29b2249b5e990d5f1
Wuping198841 <374381410@qq.com>
2019-01-11 10:31:40 +0800

    modify by chinese at 2019/1/11

--- aaa/sss.py
+++ aaa/sss.py
@@ -2,8 +2,7 @@ import selenium,time
 from selenium import webdriver
 
 driver=webdriver.Firefox()
-driver.get("http://www.baidu.com")
-driver.find_element_by_id("kw").send_keys(u"haha哈哈")
+driver.get("https://www.baidu.com")
+driver.find_element_by_id("kw").send_keys("selenuim")
 time.sleep(4)
-print(u"我爱你python")
-#
+print("123")
