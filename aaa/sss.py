0c9fc1b2d2b9b0934db09a9444e46827208b84d6
Wuping198841 <374381410@qq.com>
2019-01-11 10:08:09 +0800

    zjtest

---
+++ aaa/sss.py
@@ -0,0 +1,8 @@
+import selenium,time
+from selenium import webdriver
+
+driver=webdriver.Firefox()
+driver.get("http://www.baidu.com")
+driver.find_element_by_id("kw").send_keys(u"haha哈哈")
+time.sleep(4)
+print("heiheihei")
